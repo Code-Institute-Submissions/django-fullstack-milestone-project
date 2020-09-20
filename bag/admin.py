d8c844cc69e5a963defdd087ccf49bf76a302886
ErikaN-ir <erika.nethaway@gmail.com>
2020-09-20 19:49:16 +0000

    Added Bonus model and admin

--- bag/admin.py
+++ bag/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from .models import Bonus
 # Register your models here.
+
+
+class BonusAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'is_active',
+        'expires_on',
+        'description',
+        'amount',
+        'created_at',
+    )
+
+
+admin.site.register(Bonus, BonusAdmin)
--- bag/models.py
+++ bag/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 # Create your models here.
+class Bonus(models.Model):
+    class Meta:
+        verbose_name_plural = 'Bonuses'
+
+    name = models.CharField(max_length=50)
+    is_active = models.BooleanField(default=False)
+    expires_on = models.DateField()
+    description = models.CharField(help_text="Describes what the amount is for on a discount by discount basis.", max_length=256)
+    amount = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(100)])
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
