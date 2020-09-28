d4ea83725ed2d9374142a9f45ed79ce003428be2
ErikaN-ir <erika.nethaway@gmail.com>
2020-09-28 21:25:34 +0000

    Removed secret key and set debug

--- your_tale_jewellery/settings.py
+++ your_tale_jewellery/settings.py
@@ -19,10 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&v=llk3ivb735m#i3zk$8l_v4+@$%z(e&us(q*v6lk*-yx8ti_'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['your-tale-jewellery.herokuapp.com', 'localhost']
 
